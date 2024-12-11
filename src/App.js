fa9c9c9040f60007f04cc6b03ca0f2b48767d90a
hisham rehab <hishamrehab309@gmail.com>
2024-12-11 03:03:10 +0200

    Set_up Porject Folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+import { Navbar, Brand, CTA, Feature } from "./components";
+import {
+  Footer,
+  Blog,
+  Header,
+  Hero,
+  Services,
+  WhatGPT3,
+  Possibility,
+} from "./containers";
+
 const App = () => {
-  return <div>GPT -3</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
