8a4b140f00998bf06391291b596ba263531bba69
hisham rehab <hishamrehab309@gmail.com>
2024-12-11 04:35:51 +0200

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./App.css";
 import { Navbar, Brand, CTA, Feature } from "./components";
 import {
   Footer,
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import "./navbar.css";
+import logo from "../assets/logo.svg";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt-3__navbar'>
+      <div className='gpt-3__navbar-links'>
+        <div className='gpt-3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
