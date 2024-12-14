2b302a7b6727bbdfe177245cf45a3dd1f8324b4b
hisham rehab <hishamrehab309@gmail.com>
2024-12-14 04:22:11 +0200

    Add  blog text

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -12,7 +12,7 @@ const Blog = () => {
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-          <Article imgUrl={blog01} date="Sep 26,2021 " />
+          <Article imgUrl={blog01} text="GPT-3 and Open  AI is the future. Let us exlore how it is?" date="Sep 26,2021 " />
         </div>
         <div className='gpt3__blog-container_groupB'>
           <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
